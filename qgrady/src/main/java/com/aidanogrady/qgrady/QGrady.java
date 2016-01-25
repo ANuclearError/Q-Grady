7e5a60ac6a99e0f61908367cfac996ab9d24d5b4
Aidan O'Grady <aidan@aidanogrady.com>
2016-01-25 11:59:53 +0000

    Implemented a better control flow for the QGrady class, ensuring that the compiler has the right file locations necessary.

--- qgrady/src/main/java/com/aidanogrady/qgrady/QGrady.java
+++ qgrady/src/main/java/com/aidanogrady/qgrady/QGrady.java
@@ -1,6 +1,10 @@
 package com.aidanogrady.qgrady;
 
 import org.apache.commons.cli.*;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * The entry point of the compiler. It handles the program arguments, to
@@ -12,27 +16,20 @@ import org.apache.commons.cli.*;
 public class QGrady {
 
     /**
-     * The main method parses the args, and begins the execution of the
-     * compiler.
-     *
-     * @param args - program args.
+     * The argument options available for this system.
      */
-    public static void main(String[] args) throws ParseException {
-        Options options = createOptions();
-        CommandLineParser parser = new DefaultParser();
-        try {
-            CommandLine line = parser.parse(options, args);
-            parseOptions(line, options);
-        } catch(ParseException e) {
-            System.err.println("Parsing failed. Reason: " + e.getMessage());
-        }
+    private Options options;
+
+    public QGrady() {
+        options = createOptions();
     }
+
 
     /**
      * Creates and returns the options for the program.
      * @return options
      */
-    private static Options createOptions() {
+    private Options createOptions() {
         Options options = new Options();
         options.addOption(Option.builder("f").longOpt("file").hasArg(true)
                 .argName("file").required(true).desc("places output to <file>")
@@ -47,27 +44,134 @@ public class QGrady {
         return options;
     }
 
+
     /**
-     * Handles the parsing of the user's options, ensuring the heirarchy of
-     * calls.
-     * @param line - the options the user has input
-     * @param options - the options the system looks for.
+     * Starts the system execution by parsing the user's arguments so that the
+     * control can be dictated.
+     *
+     * @param args - program arguments
      */
-    private static void parseOptions(CommandLine line, Options options) {
-        if(line.hasOption("h")) {   // Display only help information
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.setOptionComparator(null);
-            formatter.printHelp("qgrady", options, true);
-        } else if(line.hasOption("v")) { // Display only version info.
-            System.out.println("qgrady (Q'Grady) v1.0");
-            System.out.println("Author: Aidan O'Grady");
-            System.out.println("4th Year project for M. Eng. Computer Science" +
-                    " at the University of Strathclyde, Glasgow");
-        } else { // Move onto actually compiling.
-            System.out.println("Input file: " + line.getOptionValue("f"));
-            if(line.hasOption("o")) { // User specified output file.
-                System.out.println("Output file: " + line.getOptionValue("o"));
+    public void start(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine line = parser.parse(options, args);
+            if(line.hasOption("h")) {
+                help();
             }
+            else if (line.hasOption("v")) {
+                version();
+            }
+            else {
+                String input = line.getOptionValue("f");
+                String output = line.getOptionValue("o");
+                File source = validateInput(input);
+                File dest = validateOutput(output, input);
+                System.out.println(source.getPath());
+                System.out.println(dest.getPath());
+            }
+        } catch(ParseException e) {
+            System.err.println("Parsing failed. Reason: " + e.getMessage());
+        } catch (InvalidFileTypeException e) {
+            System.err.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
         }
+    }
+
+
+    /**
+     * Displays the help message associated with the usage of the arguments.
+     */
+    private void help() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setOptionComparator(null);
+        formatter.printHelp("qgrady", options, true);
+    }
+
+
+    /**
+     * Displays the version information of the software.
+     */
+    private void version() {
+        System.out.println("qgrady (Q'Grady) v0.0");
+        System.out.println("Author: Aidan O'Grady");
+        System.out.println("4th Year project for M. Eng. Computer Science" +
+                " at the University of Strathclyde, Glasgow");
+    }
+
+
+    /**
+     * Given the location of the input, returns the File the input locates if it
+     * is valid.
+     *
+     * @param input the location of the input file.
+     * @return input file
+     * @throws FileNotFoundException - If the input file does not exist.
+     * @throws InvalidFileTypeException - If the input file is not right.
+     */
+    private File validateInput(String input) throws FileNotFoundException,
+            InvalidFileTypeException
+    {
+        // We must ensure the file exists.
+        File source = new File(input);
+        if(!source.exists()) {
+            throw new FileNotFoundException(input + ": no such file.");
+        }
+
+        // We must ensure that the file is a valid Q'Grady file.
+        String extension = FilenameUtils.getExtension(input);
+        if(source.isDirectory() || !extension.equals("qgrady")) {
+            throw new InvalidFileTypeException(
+                    input + ": not recognized as .qgrady file"
+            );
+        }
+
+        return source;
+    }
+
+
+    /**
+     * Given the location of the user's desired destination .prism file, returns
+     * the file if found. If the user did not provide an option, the given
+     * input location is used to create a destination location instead.
+     *
+     * @param output the location of the user's desired destination.
+     * @param input the location of the input file for if there is no output.
+     * @return the output file
+     * @throws FileNotFoundException - If the input file does not exist.
+     * @throws InvalidFileTypeException - If the input file is not right.
+     */
+    private File validateOutput(String output, String input) throws
+            FileNotFoundException, InvalidFileTypeException
+    {
+        // If the user did not provide a destination file, we will create one.
+        if(output == null) {
+            output = FilenameUtils.removeExtension(input);
+            output += ".prism";
+        }
+
+        // We must ensure that the destination is not a directory and is a prism
+        // file.
+        File dest = new File(output);
+        String extension = FilenameUtils.getExtension(output);
+        if(dest.isDirectory() || !extension.equals("prism")) {
+            throw new InvalidFileTypeException(
+                    output + ": not recognized as .prism file"
+            );
+        }
+
+        return dest;
+    }
+
+
+    /**
+     * The main method parses the args, and begins the execution of the
+     * compiler.
+     *
+     * @param args - program args.
+     */
+    public static void main(String[] args) throws ParseException {
+        QGrady qGrady = new QGrady();
+        qGrady.start(args);
     }
 }
