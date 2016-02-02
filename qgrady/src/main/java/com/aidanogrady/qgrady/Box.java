dfc3536ee1e7ca7ef9924f9e18686f31d11699cf
Aidan O'Grady <aidan@aidanogrady.com>
2016-02-02 13:11:23 +0000

    Added JUnit to dependancies and edited box to include probability methods.

--- qgrady/src/main/java/com/aidanogrady/qgrady/Box.java
+++ qgrady/src/main/java/com/aidanogrady/qgrady/Box.java
@@ -24,5 +24,35 @@ public class Box {
         this.box = box;
     }
 
+    /**
+     * Returns of the probability of the given output being produced by the
+     * given input.
+     *
+     * @param input - input being exmained
+     * @param output - outpu being examined
+     * @return p(output, input)
+     */
+    public double prob(int[] input, int[] output) {
+        int in = 0;
+        for (int i : input) {
+            in = in * 2 + i;
+        }
+        int out = 0;
+        for (int o : output) {
+            out = out * 2 + o;
+        }
+        return box[in][out];
+    }
 
+    /**
+     * Returns of the probability of the given output being produced by the
+     * given input.
+     *
+     * @param input - input being exmained
+     * @param output - outpu being examined
+     * @return p(output, input)
+     */
+    public double prob(int input, int output) {
+        return 0.5;
+    }
 }
