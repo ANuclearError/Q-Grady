8b77d988e79dd6cd5d64cfa90a2d2616348af5ea
Aidan O'Grady <aidan@aidanogrady.com>
2016-02-19 15:12:52 +0000

    I now have the inputs being randomly chosen in the model. Need to add the syncs to make them reset to -1

--- qgrady/src/main/java/com/aidanogrady/qgrady/FileGenerator.java
+++ qgrady/src/main/java/com/aidanogrady/qgrady/FileGenerator.java
@@ -2,6 +2,7 @@ package com.aidanogrady.qgrady;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -85,29 +86,64 @@ public class FileGenerator {
     private List<String> inputLines() {
         List<String> inputLines = new ArrayList<>();
 
-        // Generate the input declarations.
+        char[] inputs = new char[box.getInputs()];
         char input = 'z';
-        for(int i = 0; i < box.getInputs(); i++) {
-            inputLines.add(0, "\t" + input + ": [-1..1] init -1;");
+        for(int i = inputs.length - 1; i > -1; i--) {
+            inputs[i] = input;
             input--;
         }
-        input++; // Reset so char starts at the last added character.
+
+        for(char in : inputs) {
+            inputLines.add(0, "\t" + in + ": [-1..1] init -1;");
+        }
 
         // Generate 'coin toss' input selection.
-        String line = "\t[] " + input + "=-1";
-        input++;
-        for(int i = 1; i < box.getInputs(); i++) {
-            line += " & " + input + "=-1";
-            input++;
+        // LHS
+        String line = "\t[] " + inputs[0] + "=-1";
+        for(int i = 1; i < inputs.length; i++) {
+            line += " & " + inputs[i] + "=-1";
         }
+
         line += " -> ";
+
+        // RHS
         int outcomes = (int) Math.pow(2, box.getInputs());
         double prob = 1.0 / outcomes;
-        for(int i = 0; i < outcomes; i++) {
-            line += prob + " : ";
+        line += prob + " : (" + inputs[0] + "'=0)";
+        for(int i = 1; i < inputs.length; i++) {
+            line += " & (" + inputs[i] + "'=0)";
+        }
+        for(int i = 1; i < outcomes; i++) {
+            int[] vals = intToBitArray(i, box.getInputs());
+            line += " + " + prob + " : (" + inputs[0] + "'=" + vals[0] + ")";
+            for(int j = 1; j < inputs.length; j++) {
+                line += " & (" + inputs[j] + "'=" + vals[j] + ")";
+            }
         }
         line += ";";
         inputLines.add(line);
         return inputLines;
+    }
+
+    /**
+     * Converts a given integer to an array of its bit representation of the
+     * given size and returns.
+     *
+     * If the given size is larger than necessary, it will simply be filled
+     * with 0s, such that intToBitArray(5, 5) returns [0, 0, 1, 0, 1].
+     *
+     * @param value  the number being converted
+     * @param size  the size the array must fill.
+     * @return array representation.
+     */
+    private int[] intToBitArray(int value, int size) {
+        int[] array = new int[size];
+        int index = size - 1;
+        while(index >= 0) {
+            array[index] = value % 2;
+            value = value / 2;
+            index--;
+        }
+        return array;
     }
 }
