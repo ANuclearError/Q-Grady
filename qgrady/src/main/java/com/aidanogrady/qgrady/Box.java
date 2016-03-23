ea5a6fd0b3fd7db85d1783e2e5ff0127c614a4c0
Aidan O'Grady <aidan@aidanogrady.com>
2016-03-23 12:58:39 +0000

    Fixed bug in reduced probability.

--- qgrady/src/main/java/com/aidanogrady/qgrady/Box.java
+++ qgrady/src/main/java/com/aidanogrady/qgrady/Box.java
@@ -203,8 +203,10 @@ public class Box {
      */
     public double prob(int inputIndex, int input, int outputIndex, int output) {
         double sum = 0.0;
-        for(int i = 0; i < probs.length; i++) {
-            for(int j = 0; j < probs.length; j++) {
+        int inMax = (int) Math.pow(inputRange, inputs.size());
+        int outMax = (int) Math.pow(outputRange, outputs.size());
+        for(int i = 0; i < inMax; i++) {
+            for(int j = 0; j < outMax; j++) {
                 int[] in = Box.intToArray(i, inputs.size(), inputRange);
                 int[] out = Box.intToArray(j, outputs.size(), outputRange);
                 if(in[inputIndex] == input  && out[outputIndex] == output)
