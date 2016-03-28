a26b260ec013558b2a0c3c441f2e46cc814f07f4
Aidan O'Grady <aidan@aidanogrady.com>
2016-03-28 21:16:08 +0100

    Added some more tests.

--- qgrady/src/test/java/BoxTest.java
+++ qgrady/src/test/java/BoxTest.java
@@ -21,8 +21,8 @@ public class BoxTest {
         List<String> inputs = new ArrayList<>();
         List<String> outputs = new ArrayList<>();
         inputs.add("x");
-        outputs.add("y");
-        inputs.add("a");
+        inputs.add("y");
+        outputs.add("a");
         outputs.add("b");
         // Perfectly valid PR box.
         pr = new double[][] {
@@ -34,6 +34,53 @@ public class BoxTest {
 
         box = new Box(pr, inputs, outputs, 2, 2);
     }
+
+    @Test
+    public void convertListTest() {
+        List<List<Double>> test = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            List<Double> list = new ArrayList<>();
+            for (int j = 0; j < 3; j++) {
+                list.add(j / 10.0);
+            }
+            test.add(list);
+        }
+        double[][] res = Box.convertList(test);
+        double[][] exp = new double[][] {
+                {0.0, 0.1, 0.2},
+                {0.0, 0.1, 0.2},
+                {0.0, 0.1, 0.2}
+        };
+        assertArrayEquals(res, exp);
+    }
+
+    @Test
+    public void testGetProbs() {
+        double[][] res = box.getProbs();
+        assertArrayEquals(res, pr);
+    }
+
+    @Test
+    public void testInputs() {
+        List<String> inputs = new ArrayList<>();
+        inputs.add("x");
+        inputs.add("y");
+        assertEquals(inputs, box.getInputs());
+        assertEquals(2, box.getNoOfInputs());
+        assertEquals(2, box.getInputRange());
+
+    }
+
+    @Test
+    public void testOutputs() {
+        List<String> outputs = new ArrayList<>();
+        outputs.add("a");
+        outputs.add("b");
+        assertEquals(outputs, box.getOutputs());
+        assertEquals(2, box.getNoOfOutputs());
+        assertEquals(2, box.getOutputRange());
+    }
+
 
     @Test
     public void probTest() {
@@ -50,10 +97,21 @@ public class BoxTest {
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
-                    // In the PR box, all probabilities of p(a|x) == 0.25
+                    // In the PR box, all probabilities of p(a|x) == 0.5
                     assertEquals(box.prob(i, j, i, k), 0.5, 0);
                 }
             }
         }
+    }
+
+    @Test
+    public void probTestThree() {
+        int[] in = {0, 0};
+        int[] out = {1, 1};
+        int index = 1;
+        assertEquals(1.0, box.normalisedProb(in, out, index), 0);
+        out = new int[] {0, 1};
+        assertEquals(0.0, box.normalisedProb(in, out, index), 0);
+
     }
 }
